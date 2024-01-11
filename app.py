d2a9c6e17edb93eadc76b809b4ffc3e1e2e044c3
jperakis <jperakis@cisco.com>
2024-01-10 20:56:56 -0600

    Updating for heroku build

--- app.py
+++ app.py
@@ -9,20 +9,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Can use this Meraki SDK for other meraki related calls!
-dashboard = meraki.DashboardAPI(os.environ['MERAKI_API_TOKEN'], output_log=False, print_console=False)
+dashboard = meraki.DashboardAPI(os.getenv['MERAKI_API_TOKEN'], output_log=False, print_console=False)
 app = Flask(__name__)
 
 # Helpers
 def triggerAlexaOffRoutine():
     url = "https://api-v2.voicemonkey.io/trigger"
-    params = {'token':os.environ['ROUTINE_API_TOKEN'],'device':'mt30'}
+    params = {'token':os.getenv['ROUTINE_API_TOKEN'],'device':'mt30'}
 
     requests.get(url, params=params)
     return
 
 def triggerAlexaWhiteRoutine():
     url = "https://api-v2.voicemonkey.io/trigger"
-    params = {'token':os.environ['ROUTINE_API_TOKEN'],'device':'mt30-long'}
+    params = {'token':os.getenv['ROUTINE_API_TOKEN'],'device':'mt30-long'}
 
     requests.get(url, params=params)
     return
@@ -32,6 +32,7 @@ def triggerAlexaWhiteRoutine():
 def handle_meraki_event():
     if request.method == 'POST':
         #debug print
+        print(request.json)
         print("Received POST event")
 
         button_name = request.json['deviceName']
@@ -48,7 +49,7 @@ def handle_meraki_event():
         if button_press_type == 'short':
             print("Button Name: " + button_name + "\n")
             print('SHORT BUTTON PRESS')
-            
+
             triggerAlexaOffRoutine()
 
             return 'Meraki Button - Short Webhook Test'
